357d73f8b458d9b47c7ba755b8e492897240573a
Sylphiann <Hydrolilium@gmail.com>
2024-09-17 12:41:54 +0700

    Initiate CI/CD Script

--- dooit/settings.py
+++ dooit/settings.py
@@ -33,7 +33,7 @@ DEBUG = True
 
 # APP_NAME = os.getenv('APP_NAME', '')
 
-CSRF_TRUSTED_ORIGINS = ['https://dooit.up.railway.app']
+# CSRF_TRUSTED_ORIGINS = ['https://dooit.up.railway.app']
 
 ALLOWED_HOSTS = ['*']
 
@@ -99,13 +99,9 @@ WSGI_APPLICATION = 'dooit.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'railway',
-        'USER': 'postgres',
-        'PASSWORD': 'Nvrr1sIYtGCYqG5vPmMV',
-        'HOST': 'containers-us-west-132.railway.app',
-        'PORT': '7549',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
