1387d0f2d45463dc0ac82def9f6facf5422ab6fa
Sylphiann <Hydrolilium@gmail.com>
2024-10-05 10:22:55 +0700

    fix: add separate test for database

--- dooit/settings.py
+++ dooit/settings.py
@@ -108,6 +108,9 @@ if PRODUCTION:
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "postgres",
         "USER": "postgres.lhwezfqfosnnqqvfozln",
+        'TEST': {
+            'NAME': 'mytestdatabase',
+        },
         "PASSWORD": "F9Hm9exxn246*ad",
         "HOST": "aws-0-ap-southeast-1.pooler.supabase.com",
         "PORT": "6543",
