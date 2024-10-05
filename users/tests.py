a3b3559e441ca21d56529f56e74faea90e3dc2dc
Sylphiann <Hydrolilium@gmail.com>
2024-10-05 10:05:59 +0700

    add unit tests for register view

--- users/tests.py
+++ users/tests.py
@@ -2,13 +2,15 @@ from unittest import mock
 from django.test import TestCase, RequestFactory, Client
 from django.urls import reverse
 from unittest import mock
-from .views import login
+from .views import *
 from .models import CustomUser
 
 
 mock_message = mock.MagicMock()
 mock_login = mock.MagicMock()
 mock_authenticate = mock.MagicMock()
+mock_form = mock.MagicMock()
+mock_populate_category = mock.MagicMock()
 
 # Create your tests here.
 class AuthViewsTest(TestCase):
@@ -33,6 +35,8 @@ class AuthViewsTest(TestCase):
         mock_message.reset_mock()
         mock_login.reset_mock()
         mock_authenticate.reset_mock()
+        mock_form.reset_mock()
+        mock_populate_category.reset_mock()
     
     def test_login_page_renders_correct_template(self):
         # Make a GET request to the login view
@@ -103,34 +107,58 @@ class AuthViewsTest(TestCase):
         # Check the response
         self.assertEqual(response.status_code, 302) # Redirects
         self.assertEqual(response['Location'], '/login/')
-        
+    
+    @mock.patch('django.contrib.messages.warning', mock_message)
     def test_register_already_authenticated(self):
-        pass
+        # Mock the requests
+        request = self.factory.post(reverse('register'))
+        request.user = mock.MagicMock(is_authenticated=True)
 
+        mock_message.side_effect = [""]
+
+        # Call the view
+        response = register(request)
+
+        # Check the response
+        self.assertEqual(response.status_code, 302) # Redirects
+        self.assertEqual(response['Location'], '/')
+
+    # @mock.patch('users.forms.UserForm.is_valid', mock_form)
+    # @mock.patch('django.contrib.messages.success', mock_message)
     def test_register_success(self):
+        # register_form = {
+        #     'company_name' : 'test_company',
+        #     'username' : 'test_username',
+        #     'email' : 'test@email.com',
+        #     'password' : 'test_password',
+        # }
+        # custom_form = UserForm(register_form)
+        # self.assertTrue(custom_form.is_valid)
+
+        # mock_form.side_effect = [register_form]
+        # mock_populate_category.side_effect = [None]
+        # mock_message.side_effect = [""]
+        # response = self.client.post(reverse('register'), register_form)
         pass
 
+    @mock.patch('users.forms.UserForm.is_valid', mock_form)
+    @mock.patch('django.contrib.messages.error', mock_message)
     def test_register_invalid_form(self):
-        pass
+        mock_form.side_effect = [False]
+        mock_message.side_effect = [""]
 
-    def test_register_non_post_request(self):
-        pass
+        response = self.client.post(reverse('register'))
 
-class AuthFormsTest(TestCase):
-    def setup():
-        pass
+        # Check the response
+        self.assertEqual(response.status_code, 302) # Redirects
+        self.assertEqual(response['Location'], '/register/')
 
-    def userform_clean_success(self):
-        pass
+    def test_register_page_renders_correct_template(self):
+        # Make a GET request to the login view
+        response = self.client.get(reverse('register'))
 
-    def userform_clean_incorrect_password(self):
-        pass
+        # Check the response status code is 200 OK
+        self.assertEqual(response.status_code, 200)
 
-class AuthModelsTest(TestCase):
-    pass
-
-class AuthSignalsTest(TestCase):
-    pass
-
-class AuthValidatorsTest(TestCase):
-    pass
+        # Check the correct template was used
+        self.assertTemplateUsed(response, 'register.html')
