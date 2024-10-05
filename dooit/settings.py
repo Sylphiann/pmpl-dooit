62c6fbadde22cab2f6e8c7c4cd0e0edccf87fbdb
adnan-mkhtr <adnanmuchtar8e01@gmail.com>
2024-10-05 13:50:23 +0700

    add test for konsultan views

--- dooit/settings.py
+++ dooit/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-9mca@gc30#6e(8$$8ak@&)tos_qmb9he6i=zneq7=+q#8!ek!5
 # Automatically determine environment by detecting if DATABASE_URL variable.
 # DATABASE_URL is provided by Heroku if a database add-on is added
 # (e.g. Heroku Postgres).
-# PRODUCTION = os.getenv('DATABASE_URL') is not None
+PRODUCTION = os.getenv("DATABASE_URL") is not None
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -97,6 +97,13 @@ WSGI_APPLICATION = "dooit.wsgi.application"
 
 DATABASES = {
     "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
+
+if PRODUCTION:
+    DATABASES["default"] = {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "postgres",
         "USER": "postgres.lhwezfqfosnnqqvfozln",
@@ -104,7 +111,6 @@ DATABASES = {
         "HOST": "aws-0-ap-southeast-1.pooler.supabase.com",
         "PORT": "6543",
     }
-}
 
 
 AUTH_USER_MODEL = "users.CustomUser"
